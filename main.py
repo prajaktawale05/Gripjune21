1d334a38c9fa61ef830cbc6908a683ba2f1a5b70
prajaktawale05 <46518396+prajaktawale05@users.noreply.github.com>
2021-06-01 19:01:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,49 +1,39 @@
-from typing import List, Any
+
+
+
+
+
 
 import cv2
-import numpy as np
-import os
 import pytesseract
-import regex as re
+from pytesseract import Output
 
-pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\praja\OneDrive\Desktop\Tesseract-OCR\\tesseract.exe'
 
-img = cv2.imread('3648476-1.jpg')
-img1=cv2.imread('3657993.jpg')
-img2=cv2.imread('3642684.jpg')
-img1=cv2.cvtColor(img1,cv2.COLOR_BGR2RGB)
-img2=cv2.cvtColor(img2,cv2.COLOR_BGR2RGB)
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Users\praja\OneDrive\Desktop\\Tesseract-OCR\\tesseract.exe'
+
+img = cv2.imread('OB.png')
+
+
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+
 print(pytesseract.image_to_string(img))
-print(pytesseract.image_to_string(img1))
-print(pytesseract.image_to_string(img2))
 
-hImg, wImg, _ = img.shape
-boxes = pytesseract.image_to_boxes(img)
-for b in boxes.splitlines():
-    # print(b)
-    b = b.split()
-    # print(b)
-    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
-hImg, wImg, _ = img1.shape
-boxes = pytesseract.image_to_boxes(img1)
-for b in boxes.splitlines():
-    # print(b)
-    b = b.split()
-    # print(b)
-    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
-hImg, wImg, _ = img2.shape
-boxes = pytesseract.image_to_boxes(img2)
-for b in boxes.splitlines():
-    # print(b)
-    b = b.split()
-    # print(b)
-    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
+def match_template(img, template):
+
+    return cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
+
+hImg,wImg,_=img.shape
+
+box=pytesseract.image_to_boxes(img)
+
+for b in box.splitlines():
 
 
+    b = b.split(' ')
 
-cv2.imshow('Result', img)
+    img = cv2.rectangle(img, (int(b[1]), hImg - int(b[2])), (int(b[3]), hImg - int(b[4])), (0, 255, 0),0)
+
+cv2.imshow('image',img)
+
 cv2.waitKey(0)
+
